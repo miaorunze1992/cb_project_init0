a801e8c289c058f3261bc478c9079052da1cee07
linlbin520 <linlbin520@gmail.com>
2023-03-16 18:41:58 +0900

    ソースコード修正しました

---
+++ StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/controller/LoginController.java
@@ -0,0 +1,19 @@
+package co.jp.cloudbyte.StudentManagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class LoginController {
+
+	@RequestMapping("userLogin")
+	public String UserLogin() {
+		return"user/register";
+	}
+
+	@RequestMapping("register")
+	public String RegisterInto() {
+		return"user/search";
+	}
+}
