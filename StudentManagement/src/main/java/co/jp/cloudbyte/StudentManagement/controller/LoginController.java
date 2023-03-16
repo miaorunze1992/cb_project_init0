0f696192c6dbea7af4d137265c40559a79d82a3e
Lixiaocao7794 <xc1007396120@gmail.com>
2023-03-16 23:48:40 +0900

    画面遷移をする。
    Signed-off-by: Lixiaocao7794 <xc1007396120@gmail.com>

--- StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/controller/LoginController.java
+++ StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/controller/LoginController.java
@@ -3,12 +3,25 @@ package co.jp.cloudbyte.StudentManagement.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import co.jp.cloudbyte.StudentManagement.form.LoginForm;
+
 @Controller
 public class LoginController {
 	@RequestMapping("userLogin")
-	public String userLogin() {
-
-		return "index";
+	public String userLogin(LoginForm loginForm) {
+		String username = "admin";
+		String password = "admin";
+		if(!username.equals(loginForm.getUsername())) {
+			return "index";
+		}
+		if(!password.equals(loginForm.getPassword())) {
+			return "index";
+		}
+//		System.out.println("接收的值 start");
+//		System.out.println(loginForm.getUsername());
+//		System.out.println(loginForm.getPassword());
+//		System.out.println("接收的值 end");
+		return "user/register";
 
 	}
 }
