e5f3a6cb18535a8704755c933d47bf6a485b3324
Lixiaocao7794 <xc1007396120@gmail.com>
2023-03-16 18:28:46 +0900

    ユーザー一覧画面を改修しました、ログイン画面とコントローラーを改修しました。

--- StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/controller/LoginController.java
+++ StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/controller/LoginController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 	@RequestMapping("userLogin")
-
 	public String userLogin() {
 
 		return "index";
