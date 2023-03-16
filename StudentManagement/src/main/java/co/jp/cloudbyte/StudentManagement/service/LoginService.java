44491d336b77a543f3c6d78c6f40b7045c955d10
Lixiaocao7794 <xc1007396120@gmail.com>
2023-03-17 00:24:05 +0900

    ユーザー一覧画面を改修した、新規登録画面を追加、サービス新規した。
    Signed-off-by: Lixiaocao7794 <xc1007396120@gmail.com>

---
+++ StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/service/LoginService.java
@@ -0,0 +1,9 @@
+package co.jp.cloudbyte.StudentManagement.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface LoginService  {
+
+	public String checkUser();
+}
---
+++ StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/service/LoginServiceImpl.java
@@ -0,0 +1,12 @@
+package co.jp.cloudbyte.StudentManagement.service;
+
+public class LoginServiceImpl implements LoginService {
+
+	@Override
+	public String checkUser() {
+
+		return null;
+
+	}
+
+}
