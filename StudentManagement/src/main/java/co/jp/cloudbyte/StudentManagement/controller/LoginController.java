2e677ad78b347ffc9ba6c096d03ce64076d2dade
Lixiaocao7794 <xc1007396120@gmail.com>
2023-03-16 18:14:24 +0900

    ログイン画面を作成　ログインコントローラーを追加
    Signed-off-by: Lixiaocao7794 <xc1007396120@gmail.com>

---
+++ StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/controller/LoginController.java
@@ -0,0 +1,15 @@
+package co.jp.cloudbyte.StudentManagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	@RequestMapping("userLogin")
+
+	public String userLogin() {
+
+		return "index";
+
+	}
+}
---
+++ StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/form/LoginForm.java
@@ -0,0 +1,10 @@
+package co.jp.cloudbyte.StudentManagement.form;
+
+import lombok.Data;
+
+@Data
+public class LoginForm {
+	private String username;
+	private String password;
+
+}
