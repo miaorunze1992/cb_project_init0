a0f2022bcab4002e05adac1698fd6658646ccec1
linlbin520 <linlbin520@gmail.com>
2023-03-17 14:12:21 +0900

    entity追加　2.UserInfoMapper インタフェース追加　3.　UserInfoMapper.xml インタフェース実装追加　4.サービスの業務改修

--- StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/service/LoginServiceImpl.java
+++ StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/service/LoginServiceImpl.java
@@ -1,31 +1,35 @@
 package co.jp.cloudbyte.StudentManagement.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
+import co.jp.cloudbyte.StudentManagement.dao.UserInfoMapper;
+import co.jp.cloudbyte.StudentManagement.entity.User;
 import co.jp.cloudbyte.StudentManagement.form.FormLogin;
 
 @Service
 public class LoginServiceImpl implements LoginService{
-
+	@Autowired
+	UserInfoMapper userInfoMapper;
 	@Override
 	public String Logincheck(Model model, FormLogin forLogin) {
-		//假数据
-		String userDB="lin";
-		String pwdDB="123";
-		// TODO 自動生成されたメソッド・スタブ
-		//判断用户是否存在，不存在时候输出：用户名不存在,请检查，原地跳转
-		if(!userDB.equals(forLogin.getUsername())){
+		//接收用户输入的用户名
+	User userInfo = userInfoMapper.selectUsername(forLogin.getUsername());
+		//判断用户不存在，输出：用户名不存在,请检查，原地跳转
+		if(userInfo==null){
 			model.addAttribute("errorMsg", "用户名不存在,请检查");
 			return "index";
 		}
-		//用户存在，密码是正确，不正确时输出：密码不正确,请重新输入，原地跳转
-		if(!pwdDB.equals(forLogin.getPassword())) {
+		//用户存在，密码不正确输出：密码不正确,请重新输入，原地跳转
+		if(!(userInfo.getPassword().equals(forLogin.getPassword()))) {
 			model.addAttribute("errorMsg", "密码不正确,请重新输入");
 			return "index";
 		}
 		//用户密码正确跳转页面 user/Search跳转,且输出数据
+		//检测是否有数据
+		System.out.println(userInfo);
+		System.out.println(userInfoMapper.selectUserAll());
 		return "user/Search";
 	}
-
 }
