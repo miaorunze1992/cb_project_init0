d070bba75437cfc64cfd8a88e436e9f13c06c771
liangmengai <mengailiang1202@gmail.com>
2023-03-17 16:54:03 +0900

    リーダーの指摘

--- StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/controller/LoginController.java
+++ StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/controller/LoginController.java
@@ -1,20 +1,28 @@
 package co.jp.cloudbyte.StudentManagement.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import co.jp.cloudbyte.StudentManagement.form.LoginForm;
+import co.jp.cloudbyte.StudentManagement.service.LoginService;
 import co.jp.cloudbyte.StudentManagement.service.LoginServiceImpl;
 
 @Controller
 public class LoginController {
 
+	@Autowired
+	LoginService loginService;
 	@RequestMapping("userLogin")
 	public String userLogin(Model model,LoginForm LoginForm) {
-		LoginServiceImpl LoginServiceImpl=new LoginServiceImpl();
-		String result=LoginServiceImpl.checkUser(model, LoginForm);
-		return result;
+//		//创建接口实现LoginServiceImpl类的对象LoginServiceImpl
+//		LoginServiceImpl LoginServiceImpl=new LoginServiceImpl();
+//		//通过对象.方法调用LoginServiceImpl的checkUser()方法，result接收
+//		String result=LoginServiceImpl.checkUser(model, LoginForm);
+//		//方法的返回值是页面，可以直接分发
+//		return result;
+		return loginService.checkUser(model,LoginForm);
 	}
 
 	@RequestMapping("register")
--- StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/service/LoginServiceImpl.java
+++ StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/service/LoginServiceImpl.java
@@ -1,22 +1,29 @@
 package co.jp.cloudbyte.StudentManagement.service;
 
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 import co.jp.cloudbyte.StudentManagement.form.LoginForm;
 
+@Service
 public class LoginServiceImpl implements LoginService{
 	@Override
 	public String checkUser(Model model,LoginForm LoginForm) {
+		//假数据
 		String usernameDB="admin";
 		String passwordDB="admin";
+		//业务逻辑
+		//查询没有此用户，原地跳转，报错。
 		if(!usernameDB.equals(LoginForm.getUsername())) {
 			model.addAttribute("errorMsg","该用户不存在");
 			return"index";
 		}
+		//有此用户，检查密码，并且报错
 		if(!passwordDB.equals(LoginForm.getPassword())) {
 			model.addAttribute("errorMsg","用户存在 密码错误");
 			return"index";
 		}
+		//用户密码正确，跳转用户一栏
 		return"userSearch";
 	}
 
