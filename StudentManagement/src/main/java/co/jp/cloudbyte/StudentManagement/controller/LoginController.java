c5e0d904a464fc08abe866d2c2409659253f3b37
zzzzzzl <zzzzzzl@foxmail.com>
2023-03-17 17:05:17 +0900

    業務とcontrollerの作成

--- StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/controller/LoginController.java
+++ StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/controller/LoginController.java
@@ -1,27 +1,52 @@
 package co.jp.cloudbyte.StudentManagement.controller;
 
+import java.util.concurrent.Flow.Publisher;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
 
 import co.jp.cloudbyte.StudentManagement.form.LoginForm;
+import co.jp.cloudbyte.StudentManagement.service.LoginService;
+import co.jp.cloudbyte.StudentManagement.service.LoginServiceImpl;
 
 @Controller
 public class LoginController {
-
-	@RequestMapping("user11")
+	@Autowired
+	LoginService loginService;
+	@RequestMapping("userLogin")
 	public String userLogin(Model model,LoginForm loginForm) {
-		String username = "admin";
-		String password = "admin";
 
-		if (!username.equals(loginForm.getUsername())) {
-			return "index";
-		}
-
-		if (!username.equals(loginForm.getPassword())) {
-			return "index";
-		}
-
-		return "user/search";
+	//	LoginServiceImpl loginServiceImpl = new LoginServiceImpl();
+	//	String result = loginServiceImpl.checkUser(model, loginForm);
+		return loginService.checkUser(model, loginForm);
 	}
+
+		public String UserRegister() {
+			return null;
+		}
 }
+
+//		String usernameDB = "admin";
+//		String passwordDB = "admin";
+//
+//		if (!usernameDB.equals(loginForm.getUsername())) {
+//			model.addAttribute("errorMsg","该用户不存在");
+//			return "index";
+//		}
+//
+//		if (!passwordDB.equals(loginForm.getPassword())) {
+//			model.addAttribute("errorMsg","用户存在,但是密码错误");
+//			return "index";
+//		}
+//		System.out.println("登录成功");
+//		return "user/search";
+
+//	@RequestMapping("register")
+//	public String useRegister() {
+//	return"user/register";
+
+
+
