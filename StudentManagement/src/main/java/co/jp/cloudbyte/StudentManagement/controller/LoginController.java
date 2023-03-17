c58d55b9b7b3f7865c68bc8badd7a22db4c7450b
Lixiaocao7794 <xc1007396120@gmail.com>
2023-03-17 14:15:11 +0900

    コントローラーを改修しました、サービスを改修しました。
    Signed-off-by: Lixiaocao7794 <xc1007396120@gmail.com>

--- StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/controller/LoginController.java
+++ StudentManagement/src/main/java/co/jp/cloudbyte/StudentManagement/controller/LoginController.java
@@ -1,21 +1,23 @@
 package co.jp.cloudbyte.StudentManagement.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import co.jp.cloudbyte.StudentManagement.form.LoginForm;
-import co.jp.cloudbyte.StudentManagement.service.LoginServiceImpl;
+import co.jp.cloudbyte.StudentManagement.service.LoginService;
 
 @Controller
 public class LoginController {
 
+@Autowired
+LoginService loginService;
 
-	@RequestMapping("userLogin")
+@RequestMapping("userLogin")
 	public String userLogin(Model model,LoginForm loginForm) {
-		LoginServiceImpl loginServiceImpl = new LoginServiceImpl();
-		String result = loginServiceImpl.checkUser(model, loginForm);
-		return result;
+
+		return loginService.checkUser(model, loginForm);
 
 	}
 	@RequestMapping("register")
